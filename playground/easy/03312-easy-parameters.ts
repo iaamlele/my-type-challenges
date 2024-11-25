b2fea7d7162a1e1506ca8aa147dfcdf431546fc2
Le Chen <LeChen11111@163.com>
2024-11-25 16:54:20 +0800

    finish question 03312

--- playground/easy/03312-easy-parameters.ts
+++ playground/easy/03312-easy-parameters.ts
@@ -20,10 +20,11 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer P) => any ? P : never;
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
+import { argv } from 'process'
 
 function foo(arg1: string, arg2: number): void {}
 function bar(arg1: boolean, arg2: { a: 'A' }): void {}
